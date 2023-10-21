a1a695b952c9d5e461c906fed5f7558bcf0001b2
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2023-10-21 13:35:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,15 @@
-// Reference link : https://wiki.wxwidgets.org/Hello_World
-// For compilers that don't support precompilation, include "wx/wx.h"
-// #include "wx/wxprec.h"
-#include "wx-3.1/wx/wxprec.h"
-#ifndef WX_PRECOMP
-// #include "wx/wx.h"
-#include "wx-3.1/wx/wx.h"
-#endif
+#include <wx/wx.h>
 
-#include "HelloWorldApp.h"
-
-int main()
+class MyApp : public wxApp
 {
-    for (int i = 0; i < 3; i++)
+public:
+    virtual bool OnInit()
     {
-        printf("%d \r\n", i);
+        wxFrame *frame = new wxFrame(NULL, wxID_ANY, "Hello, wxWidgets");
+        wxStaticText *text = new wxStaticText(frame, wxID_ANY, "Hello, World!", wxPoint(10, 10));
+        frame->Show(true);
+        return true;
     }
+};
 
-    printf("Hello world\r\n");
-
-    return 0;
-}
-
-// IMPLEMENT_APP(HelloWorldApp)
-
-// // This is executed upon startup, like 'main()' in non-wxWidgets programs.
-// bool HelloWorldApp::OnInit()
-// {
-//     wxFrame *frame = new wxFrame((wxFrame *)NULL, -1, _T("Hello wxWidgets World"));
-//     frame->CreateStatusBar();
-//     frame->SetStatusText(_T("Hello World"));
-//     frame->Show(true);
-//     SetTopWindow(frame);
-//     return true;
-// }
+wxIMPLEMENT_APP(MyApp);
